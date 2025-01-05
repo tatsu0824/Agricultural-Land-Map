4f76578c3a78683e87ed1415e49f4043f08b5cc6
TatsuyaTakabe <119911416+tatsu0824@users.noreply.github.com>
2025-01-06 01:10:44 +0900

    Update constant.js

--- constant.js
+++ constant.js
@@ -1,2 +1,2 @@
-const API_KEY = 'AIzaSyA28dISjXwKZD8hKengt1iJzA3g57-tUz8';
+const API_KEY = 'AIzaSyAkSgFV_KFMsDDtmUicp9udYCiTAeIVR5o';
 const SHEET_ID = '1d2Nqzht-0CZiLu5HTtz3Xu8EoBidxF31K9BxobooriQ';
